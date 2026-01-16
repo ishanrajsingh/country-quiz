8e5831d80678a48aeb3c4fec17d386261dcc68a9
ISHAN RAJ SINGH <srajishan21@gmail.com>
2026-01-16 22:46:38 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">Country Quizz</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
